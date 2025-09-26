9d78107d95140784f823bd4905cf8e8305b486eb
Mecs8 <mattias.skarnell@gmail.com>
2025-09-26 13:22:04 +0200

    PlayerMovement kod

--- Fix_The_Breaker/Assets/Matte/Scripts/PlayerMovement.cs
+++ Fix_The_Breaker/Assets/Matte/Scripts/PlayerMovement.cs
@@ -2,27 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float Horizontal;
-    private float Speed = 8f;
-    private float JumpingPower = 16f;
-    private bool IsFacingRight = true;
+    public float Speed = 8f;
+    private Vector2 moveDirection;
 
-    private Rigidbody2D rb;
-    private Transform groundCheck;
-    private LayerMask groundLayer;
+    public Rigidbody2D rb;
 
-    void Start()
-    {
-        rb.freezeRotation = true;
-    }
+
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
+        float MouseX = Input.GetAxisRaw("Horizontal");
+        float MouseY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(MouseX, MouseY).normalized;
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(Horizontal * Speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveDirection.x * Speed, moveDirection.y * Speed);
     }
 }
  
