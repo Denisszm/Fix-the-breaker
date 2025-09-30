2464a83c1bb9232962f61a12f52fe130eb6f9ee5
bamboombox <olafslazynski@gmail.com>
2025-09-30 13:48:36 +0200

    Lite paralax
    skalan på  splearen ändras

--- Fix_The_Breaker/Assets/Olaf/Scripts_Olf/Olaf_Player_Movement.cs
+++ Fix_The_Breaker/Assets/Olaf/Scripts_Olf/Olaf_Player_Movement.cs
@@ -7,13 +7,19 @@ public class Olaf_Player_Movement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+
     // Update is called once per frame
     void Update()
     {
-        float MouseX = Input.GetAxisRaw("Horizontal");
-        float MouseY = Input.GetAxisRaw("Vertical");
+        float MovementX = Input.GetAxisRaw("Horizontal");
+        float MovementY = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(MouseX, MouseY).normalized;
+        moveDirection = new Vector2(MovementX, MovementY).normalized;
+
+        if (MovementY > 0 || MovementY < 0)
+        {
+            transform.localScale = new Vector3(rb.position.y * 0.5f, rb.position.y * 0.5f, 1);
+        }
     }
     private void FixedUpdate()
     {
