601ca8a060e13900e8e7f3b29600142ad45b578d
Denisszm <stefanszm@yahoo.com>
2025-10-17 13:40:45 +0200

    More variables!!!
    I added more variables to be used in the room generation and such.

--- Fix_The_Breaker/Assets/Denis/Room generation.cs
+++ Fix_The_Breaker/Assets/Denis/Room generation.cs
@@ -1,14 +1,23 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 public class NewEmptyCSharpScript
 {
     class Roomgeneration : MonoBehaviour
     {
-         
+        [SerializeField] private int roomCount = 5;
+        private List<Rooms> rooms = new List<Rooms>();
+
+        void Start()
+        {
+            
+        }
     }
     class Rooms
     {
-        [SerializeField] private Vector2 pos = new Vector2();
+        [SerializeField] public Vector2Int position;
+        public Vector2Int size;
+        public List<Vector2Int> tiles = new List<Vector2Int>();
 
         public enum Type
         {
