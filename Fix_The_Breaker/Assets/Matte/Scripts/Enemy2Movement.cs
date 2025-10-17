b491e259ecd9ad5bd84542f041881d53f417ab62
Mecs8 <mattias.skarnell@gmail.com>
2025-10-17 13:41:45 +0200

    Fixa En bättre enemy kod

--- Fix_The_Breaker/Assets/Matte/Scripts/Enemy2Movement.cs
+++ Fix_The_Breaker/Assets/Matte/Scripts/Enemy2Movement.cs
@@ -2,39 +2,126 @@ using UnityEngine;
 
 public class Enemy2Movement : MonoBehaviour
 {
-    public Transform playerBody;
-    public float speed;
+    #region Public Settings
+    [Header("Enemy Settings")]
+    public Transform raycast;
+    public LayerMask raycastMask;
+    public float raycastLength;
+    public float attackDistance;
+    public float moveSpeed;
+    public float timer;
+    #endregion
 
+    #region Private Variables
+    private RaycastHit2D hit;
+    private GameObject target;
     private float distance;
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+    #endregion
+
+
 
     private SpriteRenderer spriteRenderer;
 
 
-    void Start()
+    void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        distance = Vector2.Distance(transform.position, playerBody.position);
-        Vector2 direction = playerBody.position - transform.position;
-        direction.Normalize();
-
-
-        //enemy fov
-        if(distance < 10)
+        if(inRange)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, playerBody.position, speed * Time.deltaTime);
+            hit = Physics2D.Raycast(raycast.position, Vector2.left, raycastLength, raycastMask);
+            RaycastDebugger();
+        }
+
+        //When Player is Detected
+        if(hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if (hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        if(inRange == false)
+        {
+            StopAttack();
         }
 
 
-        //Sprite run around
+
+        //Sprite turn around
+        /*Vector2 direction = raycast.position - transform.position;
+        direction.Normalize();
+
         if (direction.x < 0)
         {
             spriteRenderer.flipX = true;
         }
         else
-            spriteRenderer.flipX = false;
+            spriteRenderer.flipX = false;*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.tag == "Player")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if (attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+    }
+
+    private void Move()
+    {
+        Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+    void Attack()
+    {
+        timer = intTimer; //Reset timer when player get in attack range
+        attackMode = true; // Check if enemy can still attack
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+
+    }
+
+    void RaycastDebugger()
+    {
+        if(distance > attackDistance)
+        {
+            Debug.DrawRay(raycast.position, Vector3.left * raycastLength, Color.red);
+        }
+        else if (attackDistance > distance)
+        {
+            Debug.DrawRay(raycast.position, Vector3.left * raycastLength, Color.green);
+        }
     }
 }
